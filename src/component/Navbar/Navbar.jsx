890cc7ff64d0cb02a4f82410c0be8b6e0be3b7d8
https://github.com/davidhakobyanf/auro.git <hakobyan.david2003@gmail.com>
2024-07-25 19:52:05 +0300

    project

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import css from './Navbar.module.css'
-const Navbar = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+import { Navbar, Nav } from 'react-bootstrap';
+import styles from './Navbar.module.css';
 
-export default Navbar;
+const NavigationBar = () => {
+    return (
+        <Navbar bg="light" expand="lg" className={styles.navbar}>
+            <Navbar.Brand href="home" className={styles.brand}>MyWebsite</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="home" className={styles.navLink}>Home</Nav.Link>
+                    <Nav.Link href="about" className={styles.navLink}>About</Nav.Link>
+                    <Nav.Link href="services" className={styles.navLink}>Services</Nav.Link>
+                    <Nav.Link href="contact" className={styles.navLink}>Contact</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+}
+
+export default NavigationBar;
