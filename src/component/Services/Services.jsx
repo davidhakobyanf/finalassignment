511aaf02190c283a7a480458a383ee4a1cd30c2a
https://github.com/davidhakobyanf/auro.git <hakobyan.david2003@gmail.com>
2024-07-27 16:40:32 +0300

    services

--- src/component/Services/Services.jsx
+++ src/component/Services/Services.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
-import css from './Services.module.css'
+import { Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import img5 from '../../images/music-701238_1920.jpg';
+import css from './Services.module.css';
+
 const Services = () => {
     return (
-        <div>
+        <div className={css.servicesSection}>
+            <Container className='py-5'>
+                <Row className="align-items-center">
+                    <Col md={6}>
+                        <img src={img5} alt="Solar Panels" className="img-fluid" />
+                    </Col>
+                    <Col md={6}>
+                        <div className={`${css.aboutText} text-white`}>
+                            <h2 className="text-primary">ABOUT US</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec elit ut mi eleifend hendrerit in a urna. Aliquam vel risus urna. Pellentesque pharetra risus quis augue ultricies, at lacinia metus imperdiet.</p>
+                            <p>Morbi dictum purus quam, vitae consectetur ipsum pulvinar sit amet. In maximus velit vel turpis tristique, eget convallis elit finibus. Aenean varius magna sit amet nulla venenatis, vel dignissim leo vestibulum.</p>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
 
         </div>
     );
